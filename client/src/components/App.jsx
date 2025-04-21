9170a0fbe996775cde3cf9b78aa244aa0d825230
Azaken1248 <suvkrm@gmail.com>
2025-04-22 02:37:01 +0530

    FIX

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -14,7 +14,7 @@ import ProtectedRoute from "./protectedRoute";
 function App() {
     return (
         <div>
-            <BrowserRouter basename="/Pocket-Justitia">
+            <BrowserRouter basename="/">
                 <GeneralProvider>
                     <Navbar />
                     <Routes>
